ad347efe53002c633dff85c844ba20d701948b7d
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-04-30 00:03:58 -0300

    Testes com novos dados e modelos de gráficos.

--- scripts/processamento.py
+++ scripts/processamento.py
@@ -3,10 +3,14 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-ano = 2024
+ano = 2022
 local = f'data_raw/vendas{ano}.csv'
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    layout="wide",
+    page_icon="📊",
+    page_title="TechWorld Insights"
+    )
 
 df = Dados(local)
 
@@ -19,5 +23,8 @@ col1.plotly_chart(fig_cat)
 fig_mes = px.bar(df.receitaMensal, x="month", y="revenue", title="Receita Mensal")
 col2.plotly_chart(fig_mes)
 
-#fig_reg = px.pie(df.receitaRegiao, values="revenue", names="customer_region", title="Receita Por Região")
-#col3.plotly_chart(fig_reg)
+fig_reg = px.pie(df.receitaRegiao.reset_index(), values="revenue", names="customer_region", title="Receita Por Região")
+col3.plotly_chart(fig_reg)
+
+fig_pay = px.pie(df.pagamentoUsado.reset_index(), values="count", names="payment_method", title="Pagamentos Mais Utilizados")
+col5.plotly_chart(fig_pay)
